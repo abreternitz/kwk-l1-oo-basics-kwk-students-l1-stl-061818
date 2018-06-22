4a0688cbc82cf7ca269ab904aae1748e3e1f5060
abreternitz <aliceburian@gmail.com>
2018-06-22 15:42:30 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -4,6 +4,9 @@ class Cats
     @fuzzy=fuzzy
     @mean=mean
   end
+  def basic_facts
+    puts "This cat has 30 Teeth, is an Obligatory Carnivore and has curved nails"
+  end
   def is_it_cute
     @cute
   end
@@ -21,3 +24,4 @@ tiger = Cats.new("very cute","pretty fuzzy","really mean")
 garfield = Cats.new("kinda","not really","yes")
   puts "Is garfield mean? #{garfield.is_it_mean}"
   puts "Is garfield fuzzy? #{garfield.is_it_fuzzy}"
+  puts "#{garfield.basic_facts}"
